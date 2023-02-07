eefaac14abb16e3d1983a9d193ecc83c3bd53030
root <root@vps.server.local>
2023-02-07 07:38:34 +0000

    Removed Fluf

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={apiResponse:""};
-  }
+function App() {
+const [resourceType, setResourceType] = useState('Home')
 
-  callAPI(){
-    fetch("http://194.163.44.139:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}));
-  }
+useEffect(() => {
+  console.log('Render')
+})
 
-  componentWillMount(){
-    this.callAPI();
-  }
-
-
-
-render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
-      <p>{this.state.apiResponse}</p>
+  <>
+    <div>
+     <button onClick={() => setResourceType('Home')}>Home</button>
+     <button onClick={() => setResourceType('Users')}>Users</button>
+     <button onClick={() => setResourceType('Comments')}>Comments</button>
     </div>
-   );
-  }
+    <h1>{resourceType}</h1>
+  </>
+  )
 }
 
 export default App;
